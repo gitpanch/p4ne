ee9c566168abffee96f04308e53e6d20a2191657
gitpanch <pavel.bry@gmail.com>
2021-04-20 18:33:59 +0300

    2day4

--- Lab1.2/table.py
+++ Lab1.2/table.py
@@ -1,3 +1,14 @@
 from matplotlib import pyplot
 from openpyxl import load_workbook
+def getvalue(x):
+    return x.value
 
+wb = load_workbook('data_analysis_lab.xlsx')
+
+cell = wb['Data']
+y = list(map(getvalue, cell['A'][1:]))
+t = list(map(getvalue, cell['C'][1:]))
+a = list(map(getvalue, cell['D'][1:]))
+pyplot.plot(y, t, label="Температура")
+pyplot.plot(y, a, label="Активность")
+pyplot.show()
