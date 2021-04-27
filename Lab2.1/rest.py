da2f21b5e5e5e59ff5c69e502b2f6bf7fdaff78f
gitpanch <pavel.bry@gmail.com>
2021-04-27 21:33:04 +0300

    13

--- Lab2.1/rest.py
+++ Lab2.1/rest.py
@@ -3,7 +3,6 @@ import requests, json, pprint
 host_ip = '10.31.70.210'
 login = 'restapi'
 password = 'j0sg1280-7@'
-
 host_url = 'https://' + host_ip + ':55443'
 
 r = requests.post(host_url + '/api/v1/auth/token-services', auth=(login, password), verify=False)
@@ -12,4 +11,21 @@ token = r.json()["token-id"]
 header = {"content-type": "application/json", "X-Auth-Token": token}
 r = requests.get(host_url + '/api/v1/interfaces', headers=header, verify=False)
 pprint.pprint(r.json())
+
+list = []
+for i in r.json()['items']:
+    list.append(i['if-name'])
+
+for l in list:
+    url = host_url +  '/api/v1/interfaces/' + l + '/statistics'
+    r = requests.get(url, headers=header, verify=False)
+    print('In packets: ', r.json()['in-total-packets'], 'out packets: ', r.json()['out-total-packets'])
+
+
+
+
+
+
+
+
 
